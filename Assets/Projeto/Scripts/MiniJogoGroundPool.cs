a6234ff894994a680b2ea8b322ff56d2efe136ac
LeandroLorran <Leandrob2612@Gmail.com>
2025-06-03 21:09:50 -0300

    vs

---
+++ Assets/Projeto/Scripts/MiniJogoGroundPool.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MiniJogoGroundPool : MiniJogoObjectPooling
+{
+    public static MiniJogoGroundPool _groundPool;
+
+    public override void Awake()
+    {
+
+        //base.Awake();
+        //base.amountToPool = //_gameControl._groundNumber;
+        //_groundPool = this;
+
+    }
+}
