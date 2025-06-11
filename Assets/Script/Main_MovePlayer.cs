16d17a8a6697de7003419c97ce34aa04ff21b6d4
j0ao-s <joaaomanoeljok@gmail.com>
2025-06-11 20:45:21 -0300

    fit refactory and comments

--- Assets/Script/Main_MovePlayer.cs
+++ Assets/Script/Main_MovePlayer.cs
@@ -3,27 +3,29 @@ using UnityEngine.InputSystem;
 
 public class Main_MovePlayer : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D _rb;
+    // Componentes
+    Rigidbody2D _rb;
 
     // Input
     Vector2 _moveInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Movimento
     [SerializeField] float _speed;
 
     void Start()
     {
+        // seta o componente rigidbody na variavel
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Acessa o game componente rigidbody e seta novas velocidades lineares
         _rb.linearVelocity = new Vector2(_moveInput.x * _speed, _moveInput.y * _speed);
     }
     public void SetMove(InputAction.CallbackContext value)
     {
-        //Debug.Log("entrou ao clicar");
+        //Ao clicar adiciona 1 no vector, ou seja, conseguimos enviar sinais quando clicar
         _moveInput = value.ReadValue<Vector2>();
     }
 }
