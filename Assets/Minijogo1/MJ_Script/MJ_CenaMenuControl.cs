a126ba0d2aa44522deefc5b2a12314128d411a74
Joao Manoel <joaaomanoeljok@gmail.com>
2025-06-19 11:30:58 -0300

    add dotween at menu mg

--- Assets/Minijogo1/MJ_Script/MJ_CenaMenuControl.cs
+++ Assets/Minijogo1/MJ_Script/MJ_CenaMenuControl.cs
@@ -1,29 +1,94 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Collections;
 
 public class MJ_CenaMenuControl : MonoBehaviour
 {
 
-    [SerializeField] Transform _panelCreditos;
+    //[SerializeField] Transform _panelCreditos;
+    [SerializeField] Transform[] _painelStart;
+    [SerializeField] Transform[] _painelCreditos;
 
 
     public void Start()
     {
-        _panelCreditos.localScale = new Vector3(0, 0, 0);
+        
+        for (int i = 0; i < _painelStart.Length; i++)
+        {
+            _painelStart[i].localScale = Vector3.zero;
+        }
+        for (int i = 0; i < _painelCreditos.Length; i++)
+        {
+            _painelCreditos[i].localScale = Vector3.zero;
+        }
+
+        PainelStartCheck(true);
+
     }
+
+    public void PainelStartCheck(bool CheckON)
+    {
+        if (CheckON == true)
+        {
+            StartCoroutine(TimeStart());
+        }
+        else
+        {
+            PainelStartOff();
+        }
+    }
+
+    IEnumerator TimeStart()
+    {
+        for (int i = 0; i < _painelStart.Length; i++)
+        {
+            _painelStart[i].DOScale(2f, .25f);
+            yield return new WaitForSeconds(0.25f);
+            _painelStart[i].DOScale(1, .25f);
+        }
+    }
+
+    IEnumerator TimeCreditos()
+    {
+        for (int i = 0; i < _painelCreditos.Length; i++)
+        {
+            _painelCreditos[i].DOScale(1.5f, .25f);
+            yield return new WaitForSeconds(0.25f);
+            _painelCreditos[i].DOScale(1, .25f);
+        }
+    }
+    public void PainelStartOff()
+    {
+        for (int i = 0; i < _painelStart.Length; i++)
+        {
+            _painelStart[i].DOScale(0, .25f);
+        }
+    }
+
     public void CenaGame()
     {
         SceneManager.LoadScene("MiniJogo1");
     }
 
-    public void CreditosOpen()
+    public void PainelCreditosCheck(bool CheckON)
     {
-        _panelCreditos.localScale = new Vector3(1, 1, 1);
+        if (CheckON == true)
+        {
+            StartCoroutine(TimeCreditos());
+        }
+        else
+        {
+            PainelConfigtOff();
+        }
     }
 
-    public void CreditosClose()
+    public void PainelConfigtOff()
     {
-        _panelCreditos.localScale = new Vector3(0, 0, 0);
+        for (int i = 0; i < _painelCreditos.Length; i++)
+        {
+            _painelCreditos[i].DOScale(0, .25f);
+        }
     }
 
     public void Kitar()
